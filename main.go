48a2df960cd3c0849035dc4c3ec0f06e55f07f77
kvnbanunu <kvnbanunu@gmail.com>
2025-10-01 15:32:20 -0700

    Updated cors middleware

--- main.go
+++ main.go
@@ -110,7 +110,11 @@ func loggingMiddleware(next http.Handler) http.Handler {
 // cors, will need to change once we set prod domain
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if os.Getenv("ENV") == "production" {
+			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_URL"))
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
