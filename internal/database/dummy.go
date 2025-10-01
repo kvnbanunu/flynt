7fbf41dd993b74bc628873619067a647fe193d0e
kvnbanunu <kvnbanunu@gmail.com>
2025-10-01 14:53:35 -0700

    Added password hash to dummy script

--- internal/database/dummy.go
+++ internal/database/dummy.go
@@ -1,16 +1,27 @@
 package database
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"flynt/internal/utils"
+)
 
 func (db *DB) InsertDummyData() error {
-	query := `
-	INSERT INTO user (name, email)
+	dummypass := os.Getenv("DUMMY_PASSWORD")
+
+	hashed, err := utils.HashPassword(dummypass)
+	if err != nil {
+		return err
+	}
+
+	query := fmt.Sprintf(`
+	INSERT INTO user (name, email, password)
 	VALUES
-	('Brandon Rada', 'brandon@gmail.com'),
-	('Evin Gonzales', 'evin@gmail.com'),
-	('Lucas Laviolette', 'lucas@gmai.com'),
-	('Kevin', 'kevin@gmail.com');
-	`
+	('Brandon Rada', 'brandon@gmail.com', '%s'),
+	('Evin Gonzales', 'evin@gmail.com', '%s'),
+	('Lucas Laviolette', 'lucas@gmai.com', '%s'),
+	('Kevin', 'kevin@gmail.com', '%s');
+	`, hashed, hashed, hashed, hashed)
 
 	if _, err := db.Exec(query); err != nil {
 		return fmt.Errorf("Failed to insert dummy data: %w", err)
