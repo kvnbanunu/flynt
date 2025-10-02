87c9629d50ed6fd7fcd8b97696fda25bfaebdeb9
kvnbanunu <kvnbanunu@gmail.com>
2025-10-01 18:59:27 -0700

    Updated dummy script to insert fyres

--- internal/database/dummy.go
+++ internal/database/dummy.go
@@ -7,6 +7,20 @@ import (
 )
 
 func (db *DB) InsertDummyData() error {
+	err := db.insertDummyUsers()
+	if err != nil {
+		return err
+	}
+
+	err = db.insertDummyFyres()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) insertDummyUsers() error {
 	dummypass := os.Getenv("DUMMY_PASSWORD")
 
 	hashed, err := utils.HashPassword(dummypass)
@@ -24,7 +38,24 @@ func (db *DB) InsertDummyData() error {
 	`, hashed, hashed, hashed, hashed)
 
 	if _, err := db.Exec(query); err != nil {
-		return fmt.Errorf("Failed to insert dummy data: %w", err)
+		return fmt.Errorf("Failed to insert dummy users: %w", err)
+	}
+
+	return nil
+}
+
+func (db *DB) insertDummyFyres() error {
+	query := `
+	INSERT INTO fyre (title, streak_count, user_id, active_days)
+	VALUES
+	('Win a game of Clash Royale', 100, 1, '10000000'),
+	('Drink water', 0, 2, '10000000'),
+	('Open Pokemon TCG pack', 9, 3, '00000001'),
+	('Go for a run', 2, 4, '00101010');
+	`
+
+	if _,err := db.Exec(query); err != nil {
+		return fmt.Errorf("Failed to insert dummy fyres: %w", err)
 	}
 
 	return nil
