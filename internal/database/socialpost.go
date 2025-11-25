69b54ab2ca1ffe5662fb99d0fa2ff4ce5c3c24a1
kvnbanunu <kvnbanunu@gmail.com>
2025-11-24 21:58:05 -0800

    fixed api post limit sql

--- internal/database/socialpost.go
+++ internal/database/socialpost.go
@@ -22,8 +22,8 @@ func (db *DB) GetAllPosts(id int) ([]FullPost, error) {
 	JOIN user u ON s.user_id = u.id
 	JOIN fyre f ON s.fyre_id = f.id
 	LEFT JOIN friend ON friend.user_id_2 = u.id AND friend.user_id_1 = ?
-	LIMIT 10
 	ORDER BY s.id DESC
+	LIMIT 10
 	`
 
 	var posts []FullPost
